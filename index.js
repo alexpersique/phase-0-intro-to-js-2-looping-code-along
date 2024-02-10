ce4c0a137db8a57c312ffdf5a51a59b48b0844b1
Alexander <alexdgenkin@gmail.com>
2024-02-10 23:16:47 +1100

    Fills index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   }
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const messages = [];
+
+let writeCards = (name, occasion) => {
+  for (let i = 0; i < name.length; i++) {
+    messages.push(`Thank you, ${name[i]}, for the wonderful ${occasion} gift!`);
+  }
+  return messages;
+};
+
+let countDown = () => {
+  let num = 10;
+  while (num >= 0) {
+    console.log(num);
+    num -= 1;
+  }
+};
+
